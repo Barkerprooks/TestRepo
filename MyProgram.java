8ed8435eaf6d67a998a3b693c76216e17fa10985
BarkerProoks <jonparkerbrooks@gmail.com>
2024-08-22 19:15:52 -0500

    first commit

---
+++ MyProgram.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MyProgram {
+
+    public static void main(String args[]) {
+        System.out.print("input your name: ");
+        Scanner input = new Scanner(System.in);
+    
+        System.out.printf("Hello, %s\n", input.nextLine());
+    }
+}
