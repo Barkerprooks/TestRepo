4f881d6ca09f99b21491a06c71434288cf95510d
BarkerProoks <jonparkerbrooks@gmail.com>
2024-08-22 19:30:28 -0500

    solved coding bat 3

--- MyProgram.java
+++ MyProgram.java
@@ -4,10 +4,23 @@ import java.util.Scanner;
 
 public class MyProgram {
 
+    // Given two int values, return their sum. 
+    // Unless the two values are the same, 
+    // then return double their sum.
+    static int sumDouble(int a, int b) {
+        
+        int c = a + b;
+
+        if (a == b) {
+            return c * 2;
+        }
+
+        return c;
+    }
+
     public static void main(String args[]) {
-        System.out.print("input your name: ");
-        Scanner input = new Scanner(System.in);
-    
-        System.out.printf("Hello, %s\n", input.nextLine());
+        System.out.println(sumDouble(3, 3));
+        System.out.println(sumDouble(3, 5));
+        System.out.println(sumDouble(2, 7));
     }
 }
