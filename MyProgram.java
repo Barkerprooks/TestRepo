d4b87b1eb8337d2f540a1d4280d2541502c94517
BarkerProoks <jonparkerbrooks@gmail.com>
2024-09-05 19:25:39 -0500

    did challenge

--- MyProgram.java
+++ MyProgram.java
@@ -3,48 +3,61 @@ import java.util.Scanner;
 
 public class MyProgram {
 
-    static int y;
-    int x;
+    static Scanner scanner = new Scanner(System.in);
 
-    public static void addNumbers(Scanner input) {
-        System.out.print("enter number 1: ");
-        int num1 = input.nextInt();
+    // Implement a program that asks the user for 
+    // the number of days. 
+    // After that, the program prints the number 
+    // of seconds in the given number of days.
 
-        System.out.print("enter number 2: ");
-        int num2 = input.nextInt();
+    // 1 day = 86400 seconds
 
-        System.out.println(num1 + num2);
+    static void challenge1() {
+        System.out.print("input days: ");
+        int days = scanner.nextInt();
+        System.out.println("seconds: " + (days * 86400));
     }
 
-    public static void subtractNumbers(Scanner input) {
-        System.out.print("enter number 1: ");
-        int num1 = input.nextInt();
+    // Write a program that asks the user 
+    // for two numbers. 
+    // After this, the program prints the sum of 
+    // the numbers given by the user.
 
-        System.out.print("enter number 2: ");
-        int num2 = input.nextInt();
+    // HINT: When you ask for multiple numbers, 
+    // create a separate variable for each:
 
-        System.out.println(num1 - num2);
+    static void challenge2() {
+        System.out.print("Give first number: ");
+        int x = scanner.nextInt();
+
+        System.out.print("Give second number: ");
+        int y = scanner.nextInt();
+
+        System.out.println("sum: " + (x + y));
+    }
+
+    // Write a program that asks the user for 
+    // two numbers and prints their
+    // sum, difference, product, and quotient. 
+
+    static void challenge3() {
+        
+        System.out.print("first number: ");
+        int x = scanner.nextInt();
+
+        System.out.print("second number: ");
+        int y = scanner.nextInt();
+
+        System.out.println("+: " + (x + y));
+        System.out.println("-: " + (x - y));
+        System.out.println("*: " + (x * y));
+        System.out.println("/: " + (x / y));
     }
 
     public static void main(String args[]) {
-
-        while (true) {
-            System.out.print("enter your input: ");
-
-            Scanner input = new Scanner(System.in);
-            String string = input.nextLine();
-
-            if (string.equals("add")) {
-                addNumbers(input);
-                continue;
-            } else if (string.equals("subtract")) {
-                subtractNumbers(input);
-                continue;
-            } else if (string.equals("exit")) {
-                break;
-            }
-
-            System.out.println(string);
-        }
+        challenge1();
+        challenge2();
+        challenge3();
     }
+
 }
